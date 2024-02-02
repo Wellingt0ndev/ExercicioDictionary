624e0683fb6398af48e2d06fcf0a87046ca1dc3d
Wellington Ferreira da Silva <108769429+Wellingt0ndev@users.noreply.github.com>
2024-02-02 12:25:44 -0300

    Solved exercise

---
+++ ExercicioDictionary/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        Dictionary<string, int> candidates = new Dictionary<string, int>();
+        while (!sr.EndOfStream)
+        {
+            string [] line = sr.ReadLine().Split(",");
+            string name = line[0];
+            int numberOfVotes = int.Parse(line[1]);
+            
+            if (candidates.ContainsKey(name))
+            {
+                candidates[name] += numberOfVotes;
+            }
+            else
+            {
+                candidates[name] = numberOfVotes;
+            }            
+        } 
+        foreach(var candidate in candidates)
+        {
+            Console.WriteLine(candidate.Key + ": " + candidate.Value);
+        }
+    }
+    
+}catch(IOException e)
+{
+    Console.WriteLine(e.Message);
+}
+
